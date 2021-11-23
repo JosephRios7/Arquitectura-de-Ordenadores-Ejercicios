6fc15c649253a5389294969f834c799ce3d8957e
JosephRios7 <94883599+JosephRios7@users.noreply.github.com>
2021-11-23 14:11:27 -0500

    Add files via upload

---
+++ EjBloque/Bloque.vhd
@@ -0,0 +1,35 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    23:04:41 11/22/2021 
+-- Design Name: 
+-- Module Name:    Bloque - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Bloque is
+port (X: in std_logic_vector (1 downto 0);
+A, B: in std_logic;
+Z: out std_logic_vector (0 to 3));
+end Bloque;
+
+architecture Behavioral of Bloque is
+
+begin
+
+
+end Behavioral;
+
---
+++ EjComposicion/Composicion.vhd
@@ -0,0 +1,62 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    23:10:12 11/22/2021 
+-- Design Name: 
+-- Module Name:    Composicion - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.std_logic_1164.all;
+work.gates.all;
+
+entity Composicion is
+port (A, B , C: in std_logic;
+F: out  std_logic );
+end Composicion;
+
+architecture e_Funcional of 
+Composicion is
+signal x: bit_vector(0 to 2);
+begin
+U0: and2 port map (B, C, x(0));
+U1: and2 port map (C, A, x(1));
+U2: and2 port map (A, B, x(2));
+U3: or3 port map (x(0), x(1), x(2), F);
+
+process(A, B)
+begin 
+	 then
+	if A = B then 
+    C <= '1' else
+	C <= '0';
+    end if;
+end process;
+process(A, B)
+begin 
+	 then
+	if A = B then 
+    C <= '1' else
+	C <= '0';
+    end if;
+end process;   
+process(A, B)
+begin 
+	 then
+	if A = B then 
+    C <= '1' else
+	C <= '0';
+    end if;
+end process;   
+end e_Funcional; 
